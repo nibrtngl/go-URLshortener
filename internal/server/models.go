4b7990b8c42d14249772ab9c947140a9b0d009f2
Nalmas <creepsfightnow@gmail.com>
2024-01-21 15:22:55 +0300

    iter7 is completed

--- internal/server/models.go
+++ internal/server/models.go
@@ -3,6 +3,7 @@ package server
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/sirupsen/logrus"
 )
 
 type ShortenRequest struct {
@@ -19,27 +20,37 @@ type Server struct {
 	Storage        map[string]string
 	App            *fiber.App
 	ShortURLPrefix string
-	Result         string `json:"URL"`
+	Result         string         `json:"URL"`
+	Logger         *logrus.Logger // Add a logger field to the Server struct
 }
 
-type ShortenResponse struct {
-	Result string `json:"result"`
+type fiberLogger struct {
+	logger *logrus.Logger
+}
+
+func (fl *fiberLogger) Write(p []byte) (n int, err error) {
+	fl.logger.Info(string(p))
+	return len(p), nil
 }
 
 func NewServer(config Config) *Server {
 	log := fiber.New()
 	log.Use(logger.New(logger.Config{
+		Output: &fiberLogger{logger: logrus.New()}, // Set the output to the custom fiberLogger
 		Format: "{\"status\": ${status}, \"duration\": \"${latency}\", \"method\": \"${method}\", \"path\": \"${path}\", \"resp\": \"${resBody}\"}\n",
 	}))
+
+	logger := logrus.New()
 
 	server := &Server{
 		Config:         config,
 		Storage:        make(map[string]string),
 		App:            log,
 		ShortURLPrefix: config.BaseURL + "/",
+		Logger:         logger, // Assign the logger to the Server struct
 	}
 
-	server.setupRoutes() // Вызов метода setupRoutes
+	server.setupRoutes()
 
 	return server
 }
